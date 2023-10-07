53f1f02272ef9ec9b2198de9d6758cc8aa3aad10
Sadeeq499 <muhammadsadeeq11223@gmail.com>
2023-10-07 22:52:59 +0500

    logo added

--- src/MainLayots/Footer.jsx
+++ src/MainLayots/Footer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Logo from "../Images/logo.jpg";
-
+import Logo from "../images/logo.png";
 function Footer() {
   return (
     <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl mt-20 md:px-24 lg:px-8">
